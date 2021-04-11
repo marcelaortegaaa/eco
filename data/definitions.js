1162ac2c23205711ab176fb5513d35412a72ee06
lordorito <maikolpedroza0@gmail.com>
2021-04-11 18:47:21 -0500

    add real word list

--- data/definitions.js
+++ data/definitions.js
@@ -1,43 +1,80 @@
-export default {
-    "Perenne": [
-        "1. adj. Continuo, incesante, que no tiene intermisión.",
-        "2. adj. Bot. Que vive más de dos años."
+{
+    "Acmé": [
+        "1. m. o f. Momento culminante. Está en el acmé de su fama. U. menos c. f.",
+        "2. m. o f. Med. Período de mayor intensidad de una enfermedad. U. menos c. f."
     ],
-    "Ojalá": [
-        "1. interj. Denota vivo deseo de que suceda algo."
+    "Arrebolada": [
+        "1. f. Conjunto de nubes enrojecidas por los rayos del sol."
     ],
-    "Luminiscencia": [
-        "1. f. Propiedad que tienen algunos cuerpos de emitir luz sin elevación de temperatura. La luminiscencia de las luciérnagas.",
-        "2. f. Luz emitida por luminiscencia."
+    "Iridiscencia": [
+        "1. f. cult. Reflejo de colores distintos, generalmente como los del arco iris."
     ],
-    "Compasión": [
-        "1. f. Sentimiento de pena, de ternura y de identificación ante los males de alguien."
+    "Melifluo": [
+        "1. adj. Que tiene miel o es parecido a ella en sus propiedades.",
+        "2. adj. Dulce, suave, delicado y tierno en el trato o en la manera de hablar. U. m. en sent. peyor."
     ],
-    "Infinito": [
-        "1. adj. Que no tiene ni puede tener fin ni término.",
-        "2. adj. Muy numeroso o enorme.",
-        "4. m. En una cámara fotográfica, última graduación de un objetivo para enfocar lo que está distante.",
-        "5. m. Mat. Valor mayor que cualquier cantidad asignable.",
-        "6. m. Mat. Signo (∞) con que se expresa el infinito."
+    "Inefable": [
+        "1. adj. Que no se puede explicar con palabras."
     ],
-    "Soledad": [
-        "1. f. Carencia voluntaria o involuntaria de compañía.",
-        "2. f. Lugar desierto, o tierra no habitada.",
-        "3. f. Pesar y melancolía que se sienten por la ausencia, muerte o pérdida de alguien o de algo.",
-        "4. f. soleá."
+    "Ensueño": [
+        "1. m. Sueño o representación fantástica de quien duerme.",
+        "2. m. Ilusión, fantasía."
     ],
-    "Resiliencia": [
-        "1. f. Capacidad de adaptación de un ser vivo frente a un agente perturbador o un estado o situación adversos.",
-        "2. f. Capacidad de un material, mecanismo o sistema para recuperar su estado inicial cuando ha cesado la perturbación a la que había estado sometido."
+    "Helénico": [
+        "1. adj. Perteneciente o relativo a Grecia o a los griegos.",
+        "2. adj. Perteneciente o relativo a la Hélade o a los antiguos helenos."
     ],
-    "Melancolía": [
-        "1. f. Tristeza vaga, profunda, sosegada y permanente, nacida de causas físicas o morales, que hace que quien la padece no encuentre gusto ni diversión en nada.",
-        "2. f. Med. Monomanía en que dominan las afecciones morales tristes.",
-        "3. f. desus. Bilis negra o atrabilis."
+    "Sosia": [
+        "1. m. y f. Persona que tiene parecido con otra hasta el punto de poder ser confundida con ella."
     ],
-    "Efervescencia": [
-        "1. f. Desprendimiento de burbujas gaseosas a través de un líquido.",
-        "2. f. Agitación, ardor, acaloramiento de los ánimos.",
-        "3. f. p. us. hervor de la sangre."
+    "Español": [
+        "1. adj. Natural de España, país de Europa. U. t. c. s.",
+        "2. adj. Perteneciente o relativo a España o a los españoles.",
+        "3. adj. Perteneciente o relativo al español (‖ lengua). Léxico español."
+    ],
+    "Tejer": [
+        "1. tr. Formar en el telar la tela con la trama y la urdimbre.",
+        "2. tr. Entrelazar hilos, cordones, espartos, etc., para formar telas, trencillas, esteras u otras cosas semejantes.",
+        "3. tr. Hacer punto a mano o con máquina tejedora.",
+        "4. tr. Dicho de ciertos animales articulados: Formar sus telas y capullos superponiendo unos hilos a otros.",
+        "5. tr. Componer, ordenar y colocar con método y disposición algo.",
+        "6. tr. Discurrir, idear un plan.",
+        "7. tr. Cruzar y descruzar brazos y pies con un orden en la danza."
+    ],
+    "Enervar": [
+        "1. tr. Debilitar, quitar las fuerzas. U. t. c. prnl.",
+        "2. tr. Debilitar la fuerza de las razones o argumentos. U. t. c. prnl.",
+        "3. tr. Poner nervioso. U. t. c. prnl."
+    ],
+    "Sociedad": [
+        "1. f. Conjunto de personas, pueblos o naciones que conviven bajo normas comunes. Viven marginados de la sociedad.",
+        "2. f. Agrupación natural o pactada de personas, organizada para cooperar en la consecución de determinados fines. Se darán ayudas a sociedades culturales.",
+        "3. f. Agrupación natural de algunos animales. Las abejas viven en sociedad.",
+        "4. f. Com. Agrupación comercial de carácter legal que cuenta con un capital inicial formado con las aportaciones de sus miembros."
+    ],
+    "Realidad": [
+        "1. f. Existencia real y efectiva de algo.",
+        "2. f. Verdad, lo que ocurre verdaderamente.",
+        "3. f. Lo que es efectivo o tiene valor práctico, en contraposición con lo fantástico e ilusorio."
+    ],
+    "Espejo": [
+        "1. m. Tabla de cristal azogado por la parte posterior, y también de acero u otro material bruñido, para que se reflejen en él los objetos que tenga delante.",
+        "2. m. Cosa que da imagen de algo. El teatro es espejo de la vida o de las costumbres.",
+        "3. m. Modelo o dechado digno de estudio e imitación. Espejo de la andante caballería.",
+        "4. m. Arq. Ventana ovalada o redonda, de pequeñas dimensiones, rodeada por lo general de adornos.",
+        "5. m. And. Transparencia de los vinos dorados."
+    ],
+    "Consciencia": [
+        "1. f. Capacidad del ser humano de reconocer la realidad circundante y de relacionarse con ella. El coma consiste en la pérdida total de la consciencia.",
+        "2. f. Conocimiento inmediato o espontáneo que el sujeto tiene de sí mismo, de sus actos y reflexiones. Perdió la consciencia de lo que le estaba pasando.",
+        "3. f. Conocimiento reflexivo de las cosas. Actuó con plena consciencia de lo que hacía.",
+        "4. f. Psicol. Acto psíquico por el que un sujeto se percibe a sí mismo en el mundo."
+    ],
+    "Fenómeno": [
+        "1. m. Toda manifestación que se hace presente a la consciencia de un sujeto y aparece como objeto de su percepción.",
+        "2. m. Cosa extraordinaria y sorprendente.",
+        "3. m. coloq. Persona o animal monstruoso.",
+        "4. m. coloq. Persona sobresaliente en su línea.",
+        "5. m. Fil. En la filosofía de Immanuel Kant, lo que es objeto de la experiencia sensible."
     ]
 }
