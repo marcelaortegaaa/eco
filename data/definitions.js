9d952e0f43b113045eca4f8d0cdb133bad2a04bd
lordorito <maikolpedroza0@gmail.com>
2021-04-11 08:58:41 -0500

    random words done

---
+++ data/definitions.js
@@ -0,0 +1,43 @@
+export default {
+    "Perenne": [
+        "1. adj. Continuo, incesante, que no tiene intermisión.",
+        "2. adj. Bot. Que vive más de dos años."
+    ],
+    "Ojalá": [
+        "1. interj. Denota vivo deseo de que suceda algo."
+    ],
+    "Luminiscencia": [
+        "1. f. Propiedad que tienen algunos cuerpos de emitir luz sin elevación de temperatura. La luminiscencia de las luciérnagas.",
+        "2. f. Luz emitida por luminiscencia."
+    ],
+    "Compasión": [
+        "1. f. Sentimiento de pena, de ternura y de identificación ante los males de alguien."
+    ],
+    "Infinito": [
+        "1. adj. Que no tiene ni puede tener fin ni término.",
+        "2. adj. Muy numeroso o enorme.",
+        "4. m. En una cámara fotográfica, última graduación de un objetivo para enfocar lo que está distante.",
+        "5. m. Mat. Valor mayor que cualquier cantidad asignable.",
+        "6. m. Mat. Signo (∞) con que se expresa el infinito."
+    ],
+    "Soledad": [
+        "1. f. Carencia voluntaria o involuntaria de compañía.",
+        "2. f. Lugar desierto, o tierra no habitada.",
+        "3. f. Pesar y melancolía que se sienten por la ausencia, muerte o pérdida de alguien o de algo.",
+        "4. f. soleá."
+    ],
+    "Resiliencia": [
+        "1. f. Capacidad de adaptación de un ser vivo frente a un agente perturbador o un estado o situación adversos.",
+        "2. f. Capacidad de un material, mecanismo o sistema para recuperar su estado inicial cuando ha cesado la perturbación a la que había estado sometido."
+    ],
+    "Melancolía": [
+        "1. f. Tristeza vaga, profunda, sosegada y permanente, nacida de causas físicas o morales, que hace que quien la padece no encuentre gusto ni diversión en nada.",
+        "2. f. Med. Monomanía en que dominan las afecciones morales tristes.",
+        "3. f. desus. Bilis negra o atrabilis."
+    ],
+    "Efervescencia": [
+        "1. f. Desprendimiento de burbujas gaseosas a través de un líquido.",
+        "2. f. Agitación, ardor, acaloramiento de los ánimos.",
+        "3. f. p. us. hervor de la sangre."
+    ]
+}
