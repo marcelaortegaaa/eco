77641658a8249203d57609179303209a98c280d0
Marcela <marcelaortega174@gmail.com>
2022-04-06 23:20:52 -0500

    sort words alphabetically and add definition number

--- data/definitions.js
+++ data/definitions.js
@@ -1,167 +1,165 @@
 export default {
-    "sempiterno": [
-        "Que durará siempre; que, habiendo tenido principio, no tendrá fin.",
-        "perpetua (|| planta).",
-        "perpetua (|| flor).",
-        "Tela de lana, basta y muy tupida, que se usaba para vestidos."
-    ],
-    "emisario": [
-        "Mensajero que se envía para indagar lo que se desea saber, para comunicar a alguien algo o para concertarse en secreto con tercera o terceras personas.",
-        "Canalización que sirve para evacuar las aguas residuales de una población hacia una depuradora o hacia el mar.",
-        "Conducto para dar salida principalmente a las aguas de un lago o de un embalse."
-    ],
-    "palma": [
-        "palmera (|| árbol).",
-        "Hoja de la palmera.",
-        "por antonom. palma que, por haber estado atada con otras en el árbol, tiene las lacinias juntas y, por falta de luz, no ha llegado a adquirir el color verde.",
-        "datilera.",
-        "palmito (|| planta).",
-        "Parte inferior y algo cóncava de la mano, desde la muñeca hasta los dedos.",
-        "Gloria, triunfo.",
-        "Botambién Cada una de las plantas angiospermas monocotiledóneas, siempre verdes, de tallo leñoso, sin ramas, recto y coronado por un penacho de grandes hojas que se parten en lacinias y se renuevan anualmente, dejando sobre el tronco la base del pecíolo. Tienen flores axilares en espiga, generalmente dioicas y muy numerosas, y fruto en drupa o baya con una semilla; poco ej., la palmera, el cocotero y el palmito. U. t. en plural como taxón.",
-        "Veter. Parte inferior del casco de las caballerías.",
-        "plural Palmadas de aplausos."
-    ],
-    "liberación": [
-        "Acción de poner en libertad.",
-        "Carta o recibo que se da al deudor cuando paga.",
-        "Cancelación o declaración de caducidad de la carga o cargas que real o aparentemente gravan un inmueble."
-    ],
-    "sufragio": [
-        "voto (|| parecer o dictamen explicado en una congregación).",
-        "Sistema electoral para la provisión de cargos.",
-        "Voto de quien tiene capacidad de elegir.",
-        "Ayuda, favor o socorro.",
-        "Obra buena que se aplica por las almas del purgatorio.",
-        "plural consuetas. U. también en singular"
-    ],
-    "avalancha": [
-        "alud."
-    ],
-    "salmón": [
-        "Pez teleósteo de hasta metro y medio de longitud, de cuerpo rollizo, cabeza apuntada y una aleta adiposa dorsal junto a la cola, de lomo azulado, vientre plateado, con reflejos irisados en los costados, que remonta los ríos para desovar, y cuya carne, rojiza y sabrosa, es muy apreciada.",
-        "Dicho de un color: Rojizo o rosado semejante al de la carne del salmón. U. también c. s. m.",
-        "De color salmón."
-    ],
-    "apocado": [
-        "De poco ánimo o espíritu.",
-        "Vil o de baja condición."
-    ],
-    "misántropo": [
-        "Dicho de una persona: Que siente o manifiesta misantropía. U. también c. s.",
-        "Que revela misantropía o da muestras de ella."
-    ],
-    "adrede": [
-        "A propósito, con intención deliberada. U. m. en sentambién peyor."
-    ],
-    "voto": [
-        "Expresión pública o secreta de una preferencia ante una opción.",
-        "Gesto, papeleta u otro objeto con que se expresa una preferencia ante una opción.",
-        "Parecer o dictamen explicado en una congregación o junta en orden a una decisión.",
-        "Persona que da o puede dar su voto.",
-        "Ruego o deprecación con que se pide a Dios una gracia.",
-        "Juramento o execración en demostración de ira.",
-        "Ofrenda dedicada a Dios o a un santo por un beneficio recibido.",
-        "deseo.",
-        "Rel. Promesa que se hace a la divinidad o a las personas santas, ya sea por devoción o para obtener determinada gracia.",
-        "Rel. Cada uno de los prometimientos que constituyen el estado religioso y tiene admitidos la Iglesia, como son la pobreza, la castidad y la obediencia."
-    ],
-    "vacuna": [
-        "Preparado de antígenos que, aplicado a un organismo, provoca en él una respuesta de defensa. U. t. en sent. fig."
-    ],
-    "insurrección": [
-        "Levantamiento, sublevación o rebelión de un pueblo, de una nación, etc."
-    ],
-    "soberanía": [
-        "Cualidad de soberano.",
-        "Poder político supremo que corresponde a un Estado independiente.",
-        "Alteza o excelencia no superada en cualquier orden inmaterial."
-    ],
-    "armisticio": [
-        "Suspensión de hostilidades pactada entre pueblos o ejércitos beligerantes."
-    ],
-    "bochinche": [
-        "Tumulto, barullo, alboroto, asonada.",
-        "Extambién buche (|| porción de líquido).",
-        "coloq. Pan. Chisme, a veces calumnioso, contra una persona o familia, que cobra mayor proporción y maledicencia a medida que pasa de una persona a otra."
-    ],
-    "sueldo": [
-        "Remuneración regular asignada por el desempeño de un cargo o servicio profesional.",
-        "Moneda, de distinto valor según los tiempos y países, igual a la vigésima parte de la libra respectiva.",
-        "sólido (|| moneda romana)."
-    ],
-    "cheque": [
-        "Mandato escrito de pago, para cobrar una cantidad determinada de los fondos que quien lo expide tiene disponibles en un banco.",
-        "Col., Cuba y Hond. Válvula que permite el paso del líquido en una sola dirección.",
-        "Hond. U. para expresar asentimiento o conformidad."
-    ],
-    "dormir": [
-        "Hallarse en el estado de reposo que consiste en la inacción o suspensión de los sentidos y de todo movimiento voluntario.",
-        "pernoctar.",
-        "Descuidarse, obrar en un negocio con menos solicitud de la que se requiere. U. m. c. prnl.",
-        "Dicho de una cosa: Ser objeto de descuido, olvido o postergación.",
-        "Dicho de lo que estaba inquieto o alterado: Sosegarse o apaciguarse. U. también c. prnl.",
-        "Dicho de una peonza o de un trompo: Bailar con mucha rapidez, sin cabecear ni moverse de un sitio.",
-        "En ciertos juegos de naipes, como el tresillo, dicho de una carta: Quedar en la baceta sin utilizar.",
-        "Tomarse tiempo para meditar o discurrir sobre cosas que den en qué pensar. U. también c. prnl.",
-        "Hacer que alguien se duerma.",
-        "dormir durante la siesta.",
-        "dormir tras una borrachera.",
-        "Cuba y El Salv. Embaucar a alguien con el fin de obtener algún beneficio.",
-        "Quedarse una persona o un animal dormidos.",
-        "Dicho de un miembro del cuerpo: adormecerse (|| entumecerse).",
-        "Mar. Dicho de la aguja de marear: Pararse y estar torpe en sus movimientos por debilidad de la imantación.",
-        "Mar. Dicho de un buque: Quedarse muy escorado por efecto del mucho viento y muy expuesto a zozobrar al menor impulso.",
-        "Méx. y R. Dom. Engañar a alguien, embaucarlo."
-    ],
-    "gota": [
-        "Pequeña porción de un líquido, con forma esferoidal.",
-        "coloq. Pizca, cantidad pequeña de algo.",
-        "Arq. Cada uno de los pequeños troncos de pirámide o de cono colocados como adorno debajo de los triglifos.",
-        "Med. Enfermedad causada por la acumulación de cristales de ácido úrico en las articulaciones de las extremidades, en las que produce hinchazón muy dolorosa.",
-        "Col. Enfermedad de ciertas plantas, como la papa, causada por un hongo que marchita el tallo.",
-        "plural Forma medicamentosa que se administra en gotas."
-    ],
-    "esnobismo": [
-        "Cualidad de esnob."
-    ],
-    "idiosincrasia": [
-        "Rasgos, temperamento, carácter, etc., distintivos y propios de un individuo o de una colectividad."
-    ],
-    "conflictuar": [
-        "Arg., Bol., Ec., Méx., Par. y Ur. Provocar un conflicto en algo o en alguien.",
-        "Arg., Bol., Ec., Méx., Par. y Ur. Dicho de una persona: Sufrir un conflicto interno o preocupación que pueden llegar a condicionar su comportamiento."
-    ],
     "abracadabrante": [
-        "Muy sorprendente y desconcertante."
-    ],
-    "bahorrina": [
-        "suciedad.",
-        "poco us. Conjunto de muchas cosas asquerosas mezcladas con agua sucia.",
-        "poco us. Conjunto de gente soez y ruin."
-    ],
-    "depauperar": [
-        "empobrecer.",
-        "Med. Debilitar, extenuar. U. m. c. prnl."
-    ],
-    "trémolo": [
-        "Mús. Sucesión rápida de muchas notas iguales, de la misma duración."
-    ],
-    "zaino": [
-        "Traidor, falso, poco seguro en el trato.",
-        "Dicho de una caballería: Que da indicios de ser falsa."
-    ],
-    "uebos": [
-        "Necesidad, cosa necesaria."
+        "1. Muy sorprendente y desconcertante."
     ],
     "acecinar": [
-        "Salar las carnes y ponerlas al humo y al aire para que, enjutas, se conserven. U. también c. prnl.",
-        "Quedarse, por vejez u otra causa, muy enjuto de carnes."
+        "1. Salar las carnes y ponerlas al humo y al aire para que, enjutas, se conserven. U. también c. prnl.",
+        "2. Quedarse, por vejez u otra causa, muy enjuto de carnes."
+    ],
+    "adrede": [
+        "1. A propósito, con intención deliberada. U. m. en sentambién peyor."
+    ],
+    "apocado": [
+        "1. De poco ánimo o espíritu.",
+        "2. Vil o de baja condición."
+    ],
+    "armisticio": [
+        "1. Suspensión de hostilidades pactada entre pueblos o ejércitos beligerantes."
+    ],
+    "avalancha": [
+        "1. Alud."
+    ],
+    "bahorrina": [
+        "1. Suciedad.",
+        "2. Poco us. Conjunto de muchas cosas asquerosas mezcladas con agua sucia.",
+        "3. Poco us. Conjunto de gente soez y ruin."
+    ],
+    "bochinche": [
+        "1. Tumulto, barullo, alboroto, asonada.",
+        "2. Extambién buche (|| porción de líquido).",
+        "3. Coloq. Pan. Chisme, a veces calumnioso, contra una persona o familia, que cobra mayor proporción y maledicencia a medida que pasa de una persona a otra."
+    ],
+    "cheque": [
+        "1. Mandato escrito de pago, para cobrar una cantidad determinada de los fondos que quien lo expide tiene disponibles en un banco.",
+        "2. Col., Cuba y Hond. Válvula que permite el paso del líquido en una sola dirección.",
+        "3. Hond. U. para expresar asentimiento o conformidad."
+    ],
+    "conflictuar": [
+        "1. Arg., Bol., Ec., Méx., Par. y Ur. Provocar un conflicto en algo o en alguien.",
+        "2. Arg., Bol., Ec., Méx., Par. y Ur. Dicho de una persona: Sufrir un conflicto interno o preocupación que pueden llegar a condicionar su comportamiento."
+    ],
+    "depauperar": [
+        "1. Empobrecer.",
+        "2. Med. Debilitar, extenuar. U. m. c. prnl."
+    ],
+    "dormir": [
+        "1. Hallarse en el estado de reposo que consiste en la inacción o suspensión de los sentidos y de todo movimiento voluntario.",
+        "2. Pernoctar.",
+        "3. Descuidarse, obrar en un negocio con menos solicitud de la que se requiere. U. m. c. prnl.",
+        "4. Dicho de una cosa: Ser objeto de descuido, olvido o postergación.",
+        "5. Dicho de lo que estaba inquieto o alterado: Sosegarse o apaciguarse. U. también c. prnl.",
+        "6. Dicho de una peonza o de un trompo: Bailar con mucha rapidez, sin cabecear ni moverse de un sitio.",
+        "7. En ciertos juegos de naipes, como el tresillo, dicho de una carta: Quedar en la baceta sin utilizar.",
+        "8. Tomarse tiempo para meditar o discurrir sobre cosas que den en qué pensar. U. también c. prnl.",
+        "9. Hacer que alguien se duerma.",
+        "10. Dormir durante la siesta.",
+        "11. Dormir tras una borrachera.",
+        "12. Cuba y El Salv. Embaucar a alguien con el fin de obtener algún beneficio.",
+        "13. Quedarse una persona o un animal dormidos.",
+        "14. Dicho de un miembro del cuerpo: adormecerse (|| entumecerse).",
+        "15. Mar. Dicho de la aguja de marear: Pararse y estar torpe en sus movimientos por debilidad de la imantación.",
+        "16. Mar. Dicho de un buque: Quedarse muy escorado por efecto del mucho viento y muy expuesto a zozobrar al menor impulso.",
+        "17. Méx. y R. Dom. Engañar a alguien, embaucarlo."
+    ],
+    "emisario": [
+        "1. Mensajero que se envía para indagar lo que se desea saber, para comunicar a alguien algo o para concertarse en secreto con tercera o terceras personas.",
+        "2. Canalización que sirve para evacuar las aguas residuales de una población hacia una depuradora o hacia el mar.",
+        "3. Conducto para dar salida principalmente a las aguas de un lago o de un embalse."
+    ],
+    "esnobismo": [
+        "1. Cualidad de esnob."
+    ],
+    "gota": [
+        "1. Pequeña porción de un líquido, con forma esferoidal.",
+        "2. Coloq. Pizca, cantidad pequeña de algo.",
+        "3. Arq. Cada uno de los pequeños troncos de pirámide o de cono colocados como adorno debajo de los triglifos.",
+        "4. Med. Enfermedad causada por la acumulación de cristales de ácido úrico en las articulaciones de las extremidades, en las que produce hinchazón muy dolorosa.",
+        "5. Col. Enfermedad de ciertas plantas, como la papa, causada por un hongo que marchita el tallo.",
+        "6. Plural Forma medicamentosa que se administra en gotas."
+    ],
+    "idiosincrasia": [
+        "1. Rasgos, temperamento, carácter, etc., distintivos y propios de un individuo o de una colectividad."
+    ],
+    "insurrección": [
+        "1. Levantamiento, sublevación o rebelión de un pueblo, de una nación, etc."
+    ],
+    "liberación": [
+        "1. Acción de poner en libertad.",
+        "2. Carta o recibo que se da al deudor cuando paga.",
+        "3. Cancelación o declaración de caducidad de la carga o cargas que real o aparentemente gravan un inmueble."
+    ],
+    "misántropo": [
+        "1. Dicho de una persona: Que siente o manifiesta misantropía. U. también c. s.",
+        "2. Que revela misantropía o da muestras de ella."
     ],
     "nefelibata": [
-        "Dicho de una persona: Soñadora, que no se apercibe de la realidad. U. también c. s."
+        "1. Dicho de una persona: Soñadora, que no se apercibe de la realidad. U. también c. s."
+    ],
+    "palma": [
+        "1. Palmera (|| árbol).",
+        "2. Hoja de la palmera.",
+        "3. Por antonom. palma que, por haber estado atada con otras en el árbol, tiene las lacinias juntas y, por falta de luz, no ha llegado a adquirir el color verde.",
+        "4. Datilera.",
+        "5. Palmito (|| planta).",
+        "6. Parte inferior y algo cóncava de la mano, desde la muñeca hasta los dedos.",
+        "7. Gloria, triunfo.",
+        "8. Botambién Cada una de las plantas angiospermas monocotiledóneas, siempre verdes, de tallo leñoso, sin ramas, recto y coronado por un penacho de grandes hojas que se parten en lacinias y se renuevan anualmente, dejando sobre el tronco la base del pecíolo. Tienen flores axilares en espiga, generalmente dioicas y muy numerosas, y fruto en drupa o baya con una semilla; poco ej., la palmera, el cocotero y el palmito. U. t. en plural como taxón.",
+        "9. Veter. Parte inferior del casco de las caballerías.",
+        "10. Plural Palmadas de aplausos."
     ],
     "plañir": [
-        "Gemir y llorar, sollozando o clamando. U. también c. prnl."
+        "1. Gemir y llorar, sollozando o clamando. U. también c. prnl."
+    ],
+    "salmón": [
+        "1. Pez teleósteo de hasta metro y medio de longitud, de cuerpo rollizo, cabeza apuntada y una aleta adiposa dorsal junto a la cola, de lomo azulado, vientre plateado, con reflejos irisados en los costados, que remonta los ríos para desovar, y cuya carne, rojiza y sabrosa, es muy apreciada.",
+        "2. Dicho de un color: Rojizo o rosado semejante al de la carne del salmón. U. también c. s. m.",
+        "3. De color salmón."
+    ],
+    "sempiterno": [
+        "1. Que durará siempre; que, habiendo tenido principio, no tendrá fin.",
+        "2. Perpetua (|| planta).",
+        "3. Perpetua (|| flor).",
+        "4. Tela de lana, basta y muy tupida, que se usaba para vestidos."
+    ],
+    "soberanía": [
+        "1. Cualidad de soberano.",
+        "2. Poder político supremo que corresponde a un Estado independiente.",
+        "3. Alteza o excelencia no superada en cualquier orden inmaterial."
+    ],
+    "sueldo": [
+        "1. Remuneración regular asignada por el desempeño de un cargo o servicio profesional.",
+        "2. Moneda, de distinto valor según los tiempos y países, igual a la vigésima parte de la libra respectiva.",
+        "3. Sólido (|| moneda romana)."
+    ],
+    "sufragio": [
+        "1. Voto (|| parecer o dictamen explicado en una congregación).",
+        "2. Sistema electoral para la provisión de cargos.",
+        "3. Voto de quien tiene capacidad de elegir.",
+        "4. Ayuda, favor o socorro.",
+        "5. Obra buena que se aplica por las almas del purgatorio.",
+        "6. Plural consuetas. U. también en singular"
+    ],
+    "trémolo": [
+        "1. Mús. Sucesión rápida de muchas notas iguales, de la misma duración."
+    ],
+    "uebos": [
+        "1. Necesidad, cosa necesaria."
+    ],
+    "vacuna": [],
+    "voto": [
+        "1. Expresión pública o secreta de una preferencia ante una opción.",
+        "2. Gesto, papeleta u otro objeto con que se expresa una preferencia ante una opción.",
+        "3. Parecer o dictamen explicado en una congregación o junta en orden a una decisión.",
+        "4. Persona que da o puede dar su voto.",
+        "5. Ruego o deprecación con que se pide a Dios una gracia.",
+        "6. Juramento o execración en demostración de ira.",
+        "7. Ofrenda dedicada a Dios o a un santo por un beneficio recibido.",
+        "8. Deseo.",
+        "9. Rel. Promesa que se hace a la divinidad o a las personas santas, ya sea por devoción o para obtener determinada gracia.",
+        "10. Rel. Cada uno de los prometimientos que constituyen el estado religioso y tiene admitidos la Iglesia, como son la pobreza, la castidad y la obediencia."
+    ],
+    "zaino": [
+        "1. Traidor, falso, poco seguro en el trato.",
+        "2. Dicho de una caballería: Que da indicios de ser falsa."
     ]
 }
